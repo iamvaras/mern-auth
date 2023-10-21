7106e1ede0c9fd458ff5a9a410354d5124172435
iamvaras <iamvaras@gmail.com>
2023-10-21 19:13:50 +0530

    Add title & favicon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { About, Home, Profile, SignIn, SignUp } from './pages'
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/profile' element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/index.jsx
@@ -0,0 +1,10 @@
+import Home from "./Home";
+import About from './About'
+import Profile from './Profile'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
+
+
+export {
+    Home, About, Profile, SignIn, SignUp
+}
